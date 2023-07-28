1f582fd97c8a49acc0eb6af53aca966d0f3c564a
Rafael Yasis <rafaelyasis@gmail.com>
2023-07-28 18:33:51 -0500

    server now works

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,13 @@
 console.log('hello from client.js');
+
+// getCalculation function
+
+
+// all the math stuff function will go here
+
+
+
+//clear the form function will go here
+
+
+//Maybe even a clear all function here?
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5001;
+// const calculatorRouter = require('./routes/calculator.router');
+const exp = require('constants');
+
+app.use(express.json());
+
+
+app.use(express.static('server/public'));
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+});
